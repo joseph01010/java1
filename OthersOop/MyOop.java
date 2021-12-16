175c0ac3c6d8b1a341b459657e245fe44c0a97d0
Joseph Kim <myunglin123@gmail.com>
2021-12-16 16:27:50 +0900

    second fresh man

--- OthersOop/MyOop.java
+++ OthersOop/MyOop.java
@@ -3,16 +3,16 @@ public class MyOop {
 		
 		Print p1 = new Print("----");
 		p1.A();
-//		p1.A();
-//		p1.B();
-//		p1.B();
-//		
-//		Print p2 = new Print();
-//		p2.delimiter = "****";
-//		p2.A();
-//		p2.A();
-//		p2.B();
-//		p2.B();
-//		
+		p1.A();
+		p1.B();
+		p1.B();
+		
+		Print p2 = new Print(null);
+		p2.delimiter = "****";
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
+		
 	}
 }
